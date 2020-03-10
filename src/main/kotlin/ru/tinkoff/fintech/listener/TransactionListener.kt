f0f48e31fd5f49fc64061cc3d4153ac96c829a22
alexander <furion1gg@gmail.com>
2020-03-10 23:09:29 +0300

    homework-3: add logging

--- src/main/kotlin/ru/tinkoff/fintech/listener/TransactionListener.kt
+++ src/main/kotlin/ru/tinkoff/fintech/listener/TransactionListener.kt
@@ -1,5 +1,6 @@
 package ru.tinkoff.fintech.listener
 
+import mu.KLogging
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 import ru.tinkoff.fintech.model.Transaction
@@ -9,10 +10,17 @@ import ru.tinkoff.fintech.service.transaction.TransactionService
 class TransactionListener(
     private val transactionService: TransactionService
 ) {
+    companion object : KLogging()
 
     @KafkaListener(topics = ["\${spring.kafka.consumer.topic}"])
     fun onMessage(transaction: Transaction) {
-        transactionService.handleTransaction(transaction)
+        logger.info("Start handle transaction: $transaction")
+        try {
+            transactionService.handleTransaction(transaction)
+            logger.info("Transaction $transaction handled successfully")
+        } catch (e: Exception) {
+            logger.error("Transaction $transaction not handled. Got error: $e")
+        }
     }
 }
 
